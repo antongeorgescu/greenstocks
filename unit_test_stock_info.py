290f23b2e2719dfa065785c99db774ca0e6d3ca7
Anton Georgesco <antongeorgescu@gmail.com>
2022-02-03 17:33:41 -0500

    added test listing sp500 stocks

--- unit_test_stock_info.py
+++ unit_test_stock_info.py
@@ -11,6 +11,7 @@ import requests_cache
 from bs4 import BeautifulSoup
 import pandas as pd
 from spacy.lang.en import English
+import pickle
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -118,7 +119,7 @@ class TestStockInfoStats(unittest.TestCase):
         parsedEx = parser(fileContent)
  
         print("-------------- entities only ---------------")
-        # if you just want the entities and nothing else, you can do access the parsed examples "ents" property like this:
+        # if want the entities and nothing else, access the parsed examples "ents" property like this:
         ents = list(parsedEx.ents)
         tags={}
         for entity in ents:
@@ -130,9 +131,45 @@ class TestStockInfoStats(unittest.TestCase):
                 tags[term].append((entity.label, entity.label_))
         print(tags)
 
+    def test_list_sp500_stocks(self):
+        #resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+        session = requests.Session()
+        session.verify = False
+
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+        resp = session.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies',headers=headers)
+
+        soup = BeautifulSoup(resp.text, 'lxml')
+        table = soup.find('table', {'class': 'wikitable sortable'})
+
+        tickers = []
+        for row in table.findAll('tr')[1:]:
+            cols = row.findAll('td')
+            ticker = cols[0].text
+            security = cols[1].text
+            sector = cols[3].text
+            subindustry = cols[4].text
+            registered = cols[6].text
+            founded = cols[8].text
+            #tickers.append(ticker.replace('\n',''))
+            tickers.append({
+                "ticker" : ticker.replace('\n',''),
+                "security" : security,
+                "sector" : sector,
+                "subindustry" : subindustry,
+                "registered" : registered,
+                "founded" : founded.replace('\n','')
+            })
+
+        dfSp500Tickers = pd.json_normalize(tickers)
+        dfSp500Tickers.to_csv(f'{self.DATA_DIR}\\sp500tickers.csv')
+        print(dfSp500Tickers.head(10))
+        
+        
 if __name__ == '__main__':
     # unittest.main(TestStockInfoStats().test_load_stock_data())
     # unittest.main(TestStockInfoStats().test_scrape_weather())
     # unittest.main(TestStockInfoStats().test_scrape_stock_news())
-    unittest.main(TestStockInfoStats().test_entity_analysis())
+    # unittest.main(TestStockInfoStats().test_entity_analysis())
+    unittest.main(TestStockInfoStats().test_list_sp500_stocks())
 
